a7b16a396c8a35f26dc2132443e8cb989e60a1e7
Tadashi Ito <10068148+itot555@users.noreply.github.com>
2021-10-08 10:34:35 +0900

    change regison default value

--- variables.tf
+++ variables.tf
@@ -72,7 +72,7 @@ variable "prefix" {
 
 variable "region" {
   description = "The region where the resources are created."
-  default     = "ap-northeast-1"
+  #default     = "ap-northeast-1"
   #default = "us-east-1"
 }
 
